0b38c1713e8d5bd35c517520c13268689caccbb4
Quentin MORVAN <quentin.morvan@accenture.com>
2020-01-13 14:00:56 +0100

    clean project

---
+++ src/components/header/Header.vue
@@ -0,0 +1,18 @@
+<template>
+  <header class="Header">
+    <h1 class="Header-title">
+      <img src="../../assets/logo.png"/>
+      Todo list
+    </h1>
+  </header>
+</template>
+
+<script>
+export default {
+  name: 'Header',
+};
+</script>
+
+<style lang="scss">
+
+</style>
