b7da34daf4856cb8a8965aa99cd4efd483446516
Quentin Mrvn <31410780+quentinmrvn@users.noreply.github.com>
2020-02-04 13:55:47 +0100

    form integration (#4)

---
+++ src/components/molecules/Form.vue
@@ -0,0 +1,19 @@
+<template>
+  <form
+    class="Form"
+    @submit.prevent="emitSubmitEvent"
+  >
+    <slot />
+  </form>
+</template>
+
+<script>
+export default {
+  name: 'Form',
+  methods: {
+    emitSubmitEvent() {
+      this.$emit('submit');
+    },
+  },
+};
+</script>
