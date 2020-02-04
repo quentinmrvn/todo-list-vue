f1049e80c319221a6adf3dc36848c8f9c7dc126f
Quentin MORVAN <quentin.morvan@accenture.com>
2020-02-04 20:38:54 +0100

    button integration

---
+++ src/components/atoms/Button.vue
@@ -0,0 +1,52 @@
+<template>
+  <button
+    class="Button"
+    v-bind="$attrs"
+    @click="emitClick"
+  >
+    {{ text }}
+  </button>
+</template>
+
+<script>
+export default {
+  name: 'Button',
+  inheritAttrs: false,
+  props: {
+    text: {
+      type: String,
+      required: true,
+    },
+  },
+  methods: {
+    emitClick() {
+      this.$emit('click');
+    },
+  },
+};
+</script>
+
+<style lang="scss">
+    .Button {
+      padding: 1rem 2rem;
+      border: none;
+      border-radius: .5rem;
+      color: $color-primary;
+      background-color: $color-secondary;
+      transition: $default-transition;
+      font-family: $font-base;
+      font-size: 1.4rem;
+
+      &:hover {
+        cursor: pointer;
+        background-color: $color-primary;
+        color: $color-secondary;
+      }
+
+      &[disabled] {
+        opacity: .5;
+        color: $color-primary;
+        background-color: $color-secondary;
+      }
+    }
+</style>
