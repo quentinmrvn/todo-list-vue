7721d83f77f694556b75816acff93f62fbb018fb
Quentin MORVAN <quentin.morvan@accenture.com>
2020-02-09 17:48:40 +0100

    finish checbkox

--- src/components/atoms/Checkbox.vue
+++ src/components/atoms/Checkbox.vue
@@ -1,12 +1,24 @@
 <template>
-  <label :for="internalId">
+  <label
+    class="Checkbox"
+    :for="internalId"
+  >
     <slot name="label">
       {{ label }}
     </slot>
+    <Icon
+      v-if="shouldBeChecked"
+      name="checkmark"
+      color="#41B882"
+      width="25"
+      height="25"
+      class="Checkbox-icon"
+    />
     <input
       :id="internalId"
       type="checkbox"
       v-bind="$attrs"
+      class="Checkbox-input"
       :checked="shouldBeChecked"
       @change.stop="handleChange"
     >
@@ -16,10 +28,43 @@
 <script>
 import checkboxBase from '../../mixins/checkboxBase';
 import inputBase from '../../mixins/inputBase';
+import Icon from './Icon.vue';
 
 export default {
   name: 'Checkbox',
+  components: { Icon },
   mixins: [checkboxBase, inputBase],
   inheritAttrs: false,
 };
 </script>
+
+<style lang="scss">
+  .Checkbox {
+    padding-left: 1.7rem;
+    position: relative;
+    font-size: 1.6rem;
+    color: $color-primary;
+
+    &:before {
+      content: '';
+      position: absolute;
+      height: 1rem;
+      width: 1rem;
+      border: .1rem solid $color-primary;
+      left: 0;
+      top: 50%;
+      transform: translateY(-50%);
+    }
+
+    &-input {
+      visibility: hidden;
+    }
+
+    &-icon {
+      left: -5px;
+      position: absolute;
+      top: 60%;
+      transform: translateY(-60%);
+    }
+  }
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,43 +1,10 @@
 <template>
-  <div class="Home">
-    <Checkbox
-      v-model="isChecked"
-      value="toto"
-      label="toto"
-      @change="checkChange"
-    />
-    <Checkbox
-      v-model="checkedBox"
-      value="titi"
-      label="titi"
-      @change="checkChange"
-    />
-    <Checkbox
-      v-model="checkedBox"
-      value="tutu"
-      label="tutu"
-      @change="checkChange"
-    />
-  </div>
+  <div class="Home" />
 </template>
 
 <script>
-import Checkbox from '../components/atoms/Checkbox.vue';
 
 export default {
   name: 'Home',
-  components: { Checkbox },
-  data() {
-    return {
-      checkedBox: [],
-      isChecked: false,
-    };
-  },
-  methods: {
-    checkChange(checked) {
-      console.log(checked);
-      console.log(this.checkedBox);
-    },
-  },
 };
 </script>
